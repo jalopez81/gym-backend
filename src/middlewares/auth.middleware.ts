2ec73324d9907b5182fe8ed37ff16ad9557362f6
Jorge L. <jorge.lopez@newfold.com>
2025-10-12 11:46:53 -0400

    agregar middleware de autenticacion

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,66 @@
+import { NextFunction, Request, Response } from "express"
+import logger from "../config/logger";
+import { verificarToken } from "../utils/jwt";
+
+declare global {
+    namespace Express {
+        interface Request {
+            usuario?: {
+                id: string;
+                email: string;
+                rol: string;
+            }
+        }
+    }
+}
+
+export const autenticar = async (req: Request, res: Response, nextFunc: NextFunction) => {
+    try {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            return res.status(401).json({
+                mensaje: 'Token no proporcionado'
+            });
+        }
+
+        const token = authHeader.substring(7)
+        const payload = verificarToken(token);
+
+        req.usuario = {
+            id: payload.id,
+            email: payload.email,
+            rol: payload.rol,
+        }
+
+
+        nextFunc();
+    } catch (error: any) {
+        logger.error("Error de autenticación:", error)
+
+
+        res.status(401).json({
+            mensaje: 'Token inválido o expirado'
+        });
+    }
+}
+
+// Middleware para verificar roles específicos
+export const autorizar = (...rolesPermitidos: string[]) => {
+    return (req: Request, res: Response, nextFunc: NextFunction) => {
+
+        if (!req.usuario) {
+            return res.status(401).json({
+                mensaje: 'Usuario no autenticado.'
+            })
+        }
+
+        if (!rolesPermitidos.includes(req.usuario.rol)) {
+            return res.status(403).json({
+                mensaje: 'Usuario no tiene permiso para acceder a este recurso'
+            })
+        }
+
+        nextFunc()
+    }
+}
