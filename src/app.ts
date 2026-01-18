a9b8a1795768bdb6e31ed5e5bc7db8693d9447e4
Jorge Lopez <jorge.lopez@newfold.com>
2026-01-18 14:42:24 -0400

    test

--- src/app.ts
+++ src/app.ts
@@ -87,7 +87,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 
-app.get('/', (req, res) => {
+app.get('/', (req:any, res:any) => {
   res.send('Hello World!')
 })
 
