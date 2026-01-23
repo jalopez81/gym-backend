ad7455e4d6f524dbe04149eeab4e25f192b20c55
Jorge L. <jorge0481rd@gmail.com>
2026-01-23 18:38:56 -0400

    remove routes and backup

--- src/app.ts
+++ src/app.ts
@@ -2,32 +2,34 @@ import express from           'express';
 import cors from              'cors';
 import dotenv from            'dotenv';
 import logger from            './config/logger'
-import asistenciaRutas from   './rutas/asistencia.rutas';
-import authRutas from         './rutas/auth.rutas'
-import backupRutas from       './rutas/backup.rutas';
-import carritoRutas from      './rutas/carrito.rutas';
-import claseRutas from        './rutas/clase.rutas';
-import configuracionRutas from './rutas/configuracion.rutas';
-import entrenadorRutas from   './rutas/entrenador.rutas'
-import devRutas from          './rutas/dev.rutas'
-import ordenRutas from        './rutas/orden.rutas';
-import planRutas from         './rutas/plan.rutas';
-import productoRutas from     './rutas/producto.rutas'
-import reporteRutas from      './rutas/reporte.rutas';
-import reservaRutas from      './rutas/reserva.rutas';
-import sesionRutas from       './rutas/sesion.rutas';
-import suscripcionRutas from  './rutas/suscripcion.rutas';
-import usuarioRutas from      './rutas/usuario.rutas'
+// import { programarBackupAutomatico } from './servicios/backup.servicio';
+// import inicializarConfiguracion from './inicializarConfiguracion';
 
-import { programarBackupAutomatico } from './servicios/backup.servicio';
+// import asistenciaRutas from   './rutas/asistencia.rutas';
+// import authRutas from         './rutas/auth.rutas'
+// import backupRutas from       './rutas/backup.rutas';
+// import carritoRutas from      './rutas/carrito.rutas';
+// import claseRutas from        './rutas/clase.rutas';
+// import configuracionRutas from './rutas/configuracion.rutas';
+// import entrenadorRutas from   './rutas/entrenador.rutas'
+// import devRutas from          './rutas/dev.rutas'
+// import ordenRutas from        './rutas/orden.rutas';
+// import planRutas from         './rutas/plan.rutas';
+// import productoRutas from     './rutas/producto.rutas'
+// import reporteRutas from      './rutas/reporte.rutas';
+// import reservaRutas from      './rutas/reserva.rutas';
+// import sesionRutas from       './rutas/sesion.rutas';
+// import suscripcionRutas from  './rutas/suscripcion.rutas';
+// import usuarioRutas from      './rutas/usuario.rutas'
+
 import { manejarErrores, rutaNoEncontrada } from './middlewares/error.middleware';
-import inicializarConfiguracion from './inicializarConfiguracion';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5001;
 
+// middlewares
 app.use(cors({
   origin: "http://localhost:3000",
   credentials: true,
@@ -36,36 +38,35 @@ app.use(cors({
 }));
 
 app.use(express.json());
-/*
-app.use('/api/asistencias',    asistenciaRutas);
-app.use('/api/auth',           authRutas)
-app.use('/api/backups',        backupRutas);
-app.use('/api/carrito',        carritoRutas);
-app.use('/api/clases',         claseRutas);
-app.use('/api/configuracion',  configuracionRutas);
-app.use('/api/entrenadores',   entrenadorRutas);
-app.use('/api/dev',            devRutas);
-app.use('/api/ordenes',        ordenRutas);
-app.use('/api/planes',         planRutas);
-app.use('/api/productos',      productoRutas)
-app.use('/api/reportes',       reporteRutas);
-app.use('/api/reservas',       reservaRutas);
-app.use('/api/sesiones',       sesionRutas);
-app.use('/api/suscripciones',  suscripcionRutas);
-app.use('/api/usuarios',       usuarioRutas)
-app.use(rutaNoEncontrada); 
+
+// app.use('/api/asistencias',    asistenciaRutas);
+// app.use('/api/auth',           authRutas)
+// app.use('/api/backups',        backupRutas);
+// app.use('/api/carrito',        carritoRutas);
+// app.use('/api/clases',         claseRutas);
+// app.use('/api/configuracion',  configuracionRutas);
+// app.use('/api/entrenadores',   entrenadorRutas);
+// app.use('/api/dev',            devRutas);
+// app.use('/api/ordenes',        ordenRutas);
+// app.use('/api/planes',         planRutas);
+// app.use('/api/productos',      productoRutas)
+// app.use('/api/reportes',       reporteRutas);
+// app.use('/api/reservas',       reservaRutas);
+// app.use('/api/sesiones',       sesionRutas);
+// app.use('/api/suscripciones',  suscripcionRutas);
+// app.use('/api/usuarios',       usuarioRutas)
+
+app.use(rutaNoEncontrada);
 app.use(manejarErrores);
-*/
 
 app.get('/status', (req, res) => {
   logger.info('Status OK');
   res.send('Status OK');
 });
 
-inicializarConfiguracion();
+// inicializarConfiguracion();
 
 app.listen(port, () => {  
-  //programarBackupAutomatico();
-
-  logger.info(`*** READY ***: Servidor corriendo en el puerto ${port}`);
+  // programarBackupAutomatico();
+  logger.info(`Servidor listo. Puerto ::: ${port}`);
 });
