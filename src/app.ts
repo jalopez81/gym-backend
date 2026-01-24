799b06b31de25b8814458aa570768beb748b5100
George L <jorge.lopez@newfold.com>
2026-01-24 10:20:35 -0400

    activar rutas

--- src/app.ts
+++ src/app.ts
@@ -5,22 +5,22 @@ import logger from            './config/logger'
 // import { programarBackupAutomatico } from './servicios/backup.servicio';
 // import inicializarConfiguracion from './inicializarConfiguracion';
 
-// import asistenciaRutas from   './rutas/asistencia.rutas';
-// import authRutas from         './rutas/auth.rutas'
-// import backupRutas from       './rutas/backup.rutas';
-// import carritoRutas from      './rutas/carrito.rutas';
-// import claseRutas from        './rutas/clase.rutas';
-// import configuracionRutas from './rutas/configuracion.rutas';
-// import entrenadorRutas from   './rutas/entrenador.rutas'
-// import devRutas from          './rutas/dev.rutas'
-// import ordenRutas from        './rutas/orden.rutas';
-// import planRutas from         './rutas/plan.rutas';
-// import productoRutas from     './rutas/producto.rutas'
-// import reporteRutas from      './rutas/reporte.rutas';
-// import reservaRutas from      './rutas/reserva.rutas';
-// import sesionRutas from       './rutas/sesion.rutas';
-// import suscripcionRutas from  './rutas/suscripcion.rutas';
-// import usuarioRutas from      './rutas/usuario.rutas'
+import asistenciaRutas from   './rutas/asistencia.rutas';
+import authRutas from         './rutas/auth.rutas'
+import backupRutas from       './rutas/backup.rutas';
+import carritoRutas from      './rutas/carrito.rutas';
+import claseRutas from        './rutas/clase.rutas';
+import configuracionRutas from './rutas/configuracion.rutas';
+import entrenadorRutas from   './rutas/entrenador.rutas'
+import devRutas from          './rutas/dev.rutas'
+import ordenRutas from        './rutas/orden.rutas';
+import planRutas from         './rutas/plan.rutas';
+import productoRutas from     './rutas/producto.rutas'
+import reporteRutas from      './rutas/reporte.rutas';
+import reservaRutas from      './rutas/reserva.rutas';
+import sesionRutas from       './rutas/sesion.rutas';
+import suscripcionRutas from  './rutas/suscripcion.rutas';
+import usuarioRutas from      './rutas/usuario.rutas'
 
 import { manejarErrores, rutaNoEncontrada } from './middlewares/error.middleware';
 
@@ -51,22 +51,22 @@ app.use(cors({
 
 app.use(express.json());
 
-// app.use('/api/asistencias',    asistenciaRutas);
-// app.use('/api/auth',           authRutas)
-// app.use('/api/backups',        backupRutas);
-// app.use('/api/carrito',        carritoRutas);
-// app.use('/api/clases',         claseRutas);
-// app.use('/api/configuracion',  configuracionRutas);
-// app.use('/api/entrenadores',   entrenadorRutas);
-// app.use('/api/dev',            devRutas);
-// app.use('/api/ordenes',        ordenRutas);
-// app.use('/api/planes',         planRutas);
-// app.use('/api/productos',      productoRutas)
-// app.use('/api/reportes',       reporteRutas);
-// app.use('/api/reservas',       reservaRutas);
-// app.use('/api/sesiones',       sesionRutas);
-// app.use('/api/suscripciones',  suscripcionRutas);
-// app.use('/api/usuarios',       usuarioRutas)
+app.use('/api/asistencias',    asistenciaRutas);
+app.use('/api/auth',           authRutas)
+app.use('/api/backups',        backupRutas);
+app.use('/api/carrito',        carritoRutas);
+app.use('/api/clases',         claseRutas);
+app.use('/api/configuracion',  configuracionRutas);
+app.use('/api/entrenadores',   entrenadorRutas);
+app.use('/api/dev',            devRutas);
+app.use('/api/ordenes',        ordenRutas);
+app.use('/api/planes',         planRutas);
+app.use('/api/productos',      productoRutas)
+app.use('/api/reportes',       reporteRutas);
+app.use('/api/reservas',       reservaRutas);
+app.use('/api/sesiones',       sesionRutas);
+app.use('/api/suscripciones',  suscripcionRutas);
+app.use('/api/usuarios',       usuarioRutas)
 
 
 app.get('/status', (req, res) => {
